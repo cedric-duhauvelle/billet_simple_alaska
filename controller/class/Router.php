18f7f8f69b3186f2a3ec0cbbb32657d74b3fda9d
cedric-duhauvelle <duhauvelle.cedric@orange.fr>
2019-05-27 11:30:43 +0200

    Création du fichier controller

---
+++ controller/class/Router.php
@@ -0,0 +1,58 @@
+<?php
+
+
+class Router{
+    
+    private $url;
+
+    public function __construct($url){
+        $this->url = $this->recoveredUrl($url);
+    }
+
+    private function recoveredUrl($url){
+        if(isset($_GET['url'])){
+            $url = explode('/', $_GET['url']);
+        }
+    }
+    public function road($page){
+        try {
+            switch ($page) {
+
+                case 'connexion':
+                    require_once '../template/connexion.php';
+                    break;
+
+                case 'inscription':
+                    require_once '../template/inscription.php';
+                    break;
+
+                case 'biographie':
+                    require_once '../template/biographie.php';
+                    break;
+
+                case 'chapitres':
+                    require_once '../template/chapitres.php';
+                    break;
+
+
+                case 'commentaires':
+                    require_once '../template/commentaires.php';
+                    break;
+
+
+                case 'contact':
+                    require_once '../template/contact.php';
+                    break;
+
+                default:
+                    require_once '../template/accueil.php';
+                    break;
+            } 
+        }
+        catch(Exception $e) {
+            echo $e->getMessage('La page n\'a pas été trouvé'), "\n";
+        }
+        
+    }
+
+}
