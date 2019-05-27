2168c7f6de4e57fc2eb0ab79c8dd85ed2450ec82
cedric-duhauvelle <duhauvelle.cedric@orange.fr>
2019-05-27 11:42:34 +0200

    Ajoute des pages biographie chapitres commentaires et contact

---
+++ template/biographie.php
@@ -0,0 +1,8 @@
+<?php 
+$title = "Biographie";
+include("header.php"); 
+?>
+<div id="content">
+    <div id="content_book"></div>
+</div>
+<?php include("footer.php"); ?>
---
+++ template/commentaires.php
@@ -0,0 +1,8 @@
+<?php 
+$title = "Commentaires";
+include("header.php"); 
+?>
+<div id="content">
+    <div id="content_book"></div>
+</div>
+<?php include("footer.php"); ?>
