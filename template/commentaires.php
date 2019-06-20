1f9423510c1584c3d46891b48d2140d4138e0cc1
cedric-duhauvelle <duhauvelle.cedric@orange.fr>
2019-06-20 17:30:13 +0200

    Ajout titre page commentaires

--- template/commentaires.php
+++ template/commentaires.php
@@ -6,6 +6,7 @@ $title = "Commentaires";
 include("header.php"); 
 ?>
 <div id="content">
+	<h2 class="title_section">Commentaires</h2>
     <div id="content_book">
         <?php
         $comment->display();
