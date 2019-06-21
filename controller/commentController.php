b0cdddde93c4ca72f3091fe6c1e1fcbdccb04aea
cedric-duhauvelle <duhauvelle.cedric@orange.fr>
2019-06-21 08:10:46 +0200

    Correction ajout commentaire dans la base de donnees

--- controller/commentController.php
+++ controller/commentController.php
@@ -3,11 +3,13 @@ require_once '../modele/Session.php';
 require_once '../modele/Comment.php';
 require_once '../modele/private/adressDataBase.php';
 
+//Recupere la page precedente
+$chapter = explode('/',$_SERVER['HTTP_REFERER']);
+var_dump($chapter[8]);
+
 //Ajout commentaire dans la base de données
 $comment = new Comment($db);
 $comment->add($_SESSION['name'], htmlspecialchars($_POST['comment']), $chapter[8]);
 
-//Recupere la page precedente
-$chapter = explode('/',$_SERVER['HTTP_REFERER']);
-var_dump($chapter[8]);
+//Redirection
 header('location: ' . $chapter[8]);
