c8e4129b927d2e6a10d61c23d804702245c9e002
cedric-duhauvelle <duhauvelle.cedric@orange.fr>
2019-07-09 15:14:32 +0200

    Correction footer

--- template/footer.php
+++ template/footer.php
@@ -4,11 +4,6 @@
             	<a href="https://twitter.com/intent/tweet/?url=http://projetformation.four.projetformationduhauvelle.fr/"><span class="fab fa-twitter" aria-hidden="true"></span></a>
         	</nav>
             <p>Copyright / Projet formation 2019</p>
-            <p>
-			    <a href="https://achecker.ca/checker/index.php?uri=referer&gid=WCAG2-AA">
-			      	<img src="https://achecker.ca/images/icon_W2_aa.jpg" alt="WCAG 2.0 (Level AA)" height="32" width="102" />
-			    </a>
-			</p>
         </footer>
     </body>
 </html>
