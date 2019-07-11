995266c9c500b73d5b8da0efff542f9db93de40a
cedric-duhauvelle <duhauvelle.cedric@orange.fr>
2019-07-11 09:09:11 +0200

    Modification index.php

--- public/index.php
+++ public/index.php
@@ -1,18 +1,20 @@
 <?php
-
 require_once '../modele/private/adressDataBase.php';
 require '../modele/Router.php';
 
-$url = 'accueil';
 if(array_key_exists('url', $_GET)) {
-    $url = htmlspecialchars($_GET['url']);
-}
+    $url = filter_var($_GET['url'], FILTER_SANITIZE_STRING);
+} else {
+	$url = 'accueil';
+}  
 
+//Gestion des erreurs
 set_exception_handler('exception');
 function exception($e)
 {
     $errorController = new CustomException($e);
 }
+
 //Appel du routeur
 $router = new Router($db);
 $router->setUrl($url);
