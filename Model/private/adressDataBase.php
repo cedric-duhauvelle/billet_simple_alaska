4196b662fe2a155f126e2eceeb7b29af9f8c4362
cedric-duhauvelle <duhauvelle.cedric@orange.fr>
2019-08-14 17:14:37 +0200

    Correction .gitignore

--- Model/private/adressDataBase.php
+++ Model/private/adressDataBase.php
@@ -1,9 +1,7 @@
 <?php
 
-namespace Model;
-
 use PDO;
-use modele\CustomException;
+use Model\CustomException;
 
 try {
 	$db = new PDO('mysql:host=localhost;dbname=billet_alaska;charset=utf8', 'Cedric', 'Billetalaska0&');
