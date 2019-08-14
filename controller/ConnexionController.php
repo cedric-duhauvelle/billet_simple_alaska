9f35505a23f2269f21ee10d4ce419e3761873bfc
cedric-duhauvelle <duhauvelle.cedric@orange.fr>
2019-08-14 16:19:39 +0200

    Correction ConnexionController.php

--- controller/ConnexionController.php
+++ controller/ConnexionController.php
@@ -4,6 +4,7 @@ namespace Controller;
 
 use Model\Router;
 use Model\Session;
+use Manager\UserManager;
 
 class ConnexionController
 {
@@ -16,27 +17,27 @@ class ConnexionController
     public function checkUser($db)
     {
         $routeur = new Router($db);
-        $check = new DataRecover($db);
+        $user = new UserManager($db);
         $session = new Session();
 
         //Nettoye la variable '$_POST'
         $postClean = $routeur->cleanArray($_POST);
 
         //Verifie le nom 
-        if ($check->recover('users', 'name', $postClean['pseudo'], 'id') === null) {
+        if ($user->checkUserData('name', $postClean['pseudo'], 'id') === null) {
             $session->addSession('errorName', 'Nom incorrect!!');
         } else {
             //verifie le password
-            if (password_verify($postClean['password'], $check->recover('users', 'name', $postClean['pseudo'], 'password'))) {
+            if (password_verify($postClean['password'], $user->checkUserData('name', $postClean['pseudo'], 'password'))) {
                 if (array_key_exists('admin', $_SESSION)) {
                     session_destroy();
                 }
-                $session->addSession('id_user', $check->recover('users', 'name', $postClean['pseudo'], 'id'));
+                $session->addSession('id_user', $user->checkUserData('name', $postClean['pseudo'], 'id'));
                 $session->addSession('name', $postClean['pseudo']);
                 if ('admin' === $postClean['pseudo']) {
                     $session->addSession('admin', 'admistrateur');
                 }
-                header('Location: profil');
+                return header('Location: profil');
             } else {
                 $session->addSession('errorPassword', 'Mot de passe incorrect!!');   
             }
